38d152c6c02badece30aa10b1a6d59dce8da7b7b
Yashodeep Kacholiya <yashodeepkacholiya@Yashodeeps-MacBook-Air.local>
2021-09-10 10:18:24 +0530

    removed warning

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ from matplotlib import pyplot
 
 # Create a new plot
 figure = pyplot.figure()
-axes = mplot3d.Axes3D(figure)
+axes = mplot3d.Axes3D(figure,auto_add_to_figure=False)
+figure.add_axes(axes)
 
 # Load the STL files and add the vectors to the plot
 your_mesh = mesh.Mesh.from_file('assignment.stl')
+print(your_mesh)
 axes.add_collection3d(mplot3d.art3d.Poly3DCollection(your_mesh.vectors))
 
 # Auto scale to the mesh size
